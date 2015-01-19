e647f3cac18a18e3e0fd8b8aaccad84243036298
Rahul Jain <rahulkj@gmail.com>
2015-01-19 12:42:13 -0600

    Added script to host the cloudfoundry docs locally

---
+++ scripts/cf-docs-site.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git_clone_update() {
+	if [ ! -d $1 ]; then
+		git clone https://github.com/cloudfoundry/$1
+	fi
+	
+	cd $1	
+	git pull
+}
+
+echo "###### Sync and host CF Docs locally ######"
+if [ $# -lt 1 ]; then
+	echo "Usage: ./cf-docs-site.sh <work-dir>"
+	printf "\t %s \t\t %s \n" "work-dir:" "Specify the work directory"
+	exit 1
+fi
+
+if [ ! -d $1 ]; then
+	logError "Non-existant directory: $2"
+fi
+
+export WORK_DIR=$1
+
+git_clone_update docs-book-cloudfoundry
+git_clone_update docs-cloudfoundry-concepts
+git_clone_update docs-dev-guide
+git_clone_update docs-cf-admin
+git_clone_update docs-services
+git_clone_update docs-deploying-cf
+git_clone_update docs-bosh
+git_clone_update docs-running-cf
+git_clone_update docs-buildpacks
+
+cd $WORK_DIR/docs-book-cloudfoundry
+rm -rf final_app/Gemfile.lock
+./run.sh
