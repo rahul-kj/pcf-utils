8f27297c32d64f0bb2937ec232b0d9b4a00a7a10
Rahul Jain <rjain@pivotal.io>
2015-07-22 13:13:14 -0500

    Updated scripts and using # as a delimiter

--- scripts/assign-users.sh
+++ scripts/assign-users.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Please update the user domain on line numnber 26, and comment lines 3 and 4, and re-run the program"
-exit 1
-
 if [ $# -lt 6 ]; then
   echo "Usage: ./assign-users.sh <api url> <admin username> <admin password> <uaa url> <admin username> <admin-secret>"
   exit 1
@@ -14,33 +11,35 @@ cf login -u $2 -p $3
 uaac target $4 >> /tmp/assign-users.log 2>&1
 uaac token client get $5 -s $6 >> /tmp/assign-users.log 2>&1
 
-IFS=", "
-while read f1 f2 f3 f4 f5 f6
+IFS="#"
+while read f1 f2 f3 f4 f5 f6 f7
 do
         echo "UID is                      : $f1"
-        echo "UserName is                 : $f2"
-        echo "OrgName  is                 : $f3"
-        echo "Org Permissions  is         : $f4"
-        echo "SpaceName is                : $f5"
-        echo "Space Permission is         : $f6"
+        echo "Email is                    : $f2"
+        echo "UserName is                 : $f3"
+        echo "OrgName  is                 : $f4"
+        echo "Org Permissions  is         : $f5"
+        echo "SpaceName is                : $f6"
+        echo "Space Permission is         : $f7"
 
-        RESPONSE=`uaac curl -H "Content-Type: application/json" -k  /Users -X POST -d '{"userName":"'"$f2"'","emails":[{"value":"'"$f2"'"}],"origin":"ldap","externalId":"'$f1',ou=People,dc=homelab,dc=io"}'`
-        ID=`echo $RESPONSE | grep "id" | head -3 | tail -1 | cut -d ":" -f2 | cut -d " " -f2`
+        RESPONSE=`uaac curl -H "Content-Type: application/json" -k  /Users -X POST -d '{"userName":"'"$f3"'","emails":[{"value":"'"$f2"'"}],"origin":"ldap","externalId":"'$f1'"}'`
+        echo $RESPONSE > user-$f1.txt
+        ID=`echo $RESPONSE | grep "id" | head -3 | tail -1 | cut -d ":" -f2 | cut -d " " -f2 | cut -d "," -f1`
         echo $ID
         cf curl /v2/users -d '{"guid":'$ID'}' -X POST >> /tmp/assign-users.log 2>&1
 
-        IFS="|" read -a orgperms <<< "$f4"
+        IFS="|" read -a orgperms <<< "$f5"
 
         for orgperm in "${orgperms[@]}"
         do
-          cf set-org-role $f2 $f3 $orgperm
+          cf set-org-role $f3 $f4 $orgperm
         done
 
-        IFS="|" read -a spaceperms <<< "$f6"
+        IFS="|" read -a spaceperms <<< "$f7"
 
         for spaceperm in "${spaceperms[@]}"
         do
-          cf set-space-role $f2 $f3 $f5 $spaceperm
+          cf set-space-role $f3 $f4 $f6 $spaceperm
         done
 
 done < users.csv
